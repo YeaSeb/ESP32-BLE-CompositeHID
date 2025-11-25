60af535cb6365292b8f6c4f0fd0129dc34d97f6f
Sebastián Marcelo zambrano Rosas <YeaSeb@users.noreply.github.com>
2025-11-25 14:01:15 -0300

    Updated LED example code
    Better handling of reports not intended for LED. Might move all that code to it's own event as part of the library after the fact, for now it works in the sketch.

--- examples/dualsenseExamples/Dualsense_Edge_Controller/Dualsense_Edge_Controller.ino
+++ examples/dualsenseExamples/Dualsense_Edge_Controller/Dualsense_Edge_Controller.ino
@@ -2,43 +2,59 @@
 
 #include <BleCompositeHID.h>
 #include <DualsenseGamepadDevice.h>
-#define CONFIG_BT_NIMBLE_EXT_ADV 0
-#define CONFIG_BT_NIMBLE_HOST_TASK_STACK_SIZE 4096
+#define CONFIG_BT_NIMBLE_EXT_ADV 1
 #define CONFIG_MTU_SIZE 430
 #define CONFIG_DEFAULT_PHY BLE_GAP_LE_PHY_2M_MASK
 
 int ledPin = 8; // LED connected to digital pin 13
 uint8_t playerLEDs = 0x00;
+uint8_t ledcolor[3] = {0,0,0}; 
+uint8_t muteled = 0;
 DualsenseGamepadDevice* dualsense;
 BleCompositeHID compositeHID("Libresteishon Edge", "YeaSeb", 100);
-void OnPlayerLEDChange(DualsenseGamepadOutputReportData data)
+
+void onLEDsetup(DualsenseGamepadOutputReportData data)
 {
-    if ((data.player_leds & DUALSENSE_PLAYERLED_ON) == DUALSENSE_PLAYERLED_ON) {
-        if ((data.player_leds & DUALSENSE_PLAYERLED_1) == DUALSENSE_PLAYERLED_1) {
-            Serial.println("led 1 on");
-        } else {
-            Serial.println("led 1 off");
+    if (data.player_leds != playerLEDs) {
+        playerLEDs=data.player_leds;
+        if ((data.player_leds & DUALSENSE_PLAYERLED_ON) == DUALSENSE_PLAYERLED_ON) {
+            if ((data.player_leds & DUALSENSE_PLAYERLED_1) == DUALSENSE_PLAYERLED_1) {
+                Serial.println("led 1 on");
+            } else {
+                Serial.println("led 1 off");
+            }
+            if ((data.player_leds & DUALSENSE_PLAYERLED_2) == DUALSENSE_PLAYERLED_2) {
+                Serial.println("led 2 on");
+            } else {
+                Serial.println("led 2 off");
+            }
+            if ((data.player_leds & DUALSENSE_PLAYERLED_3) == DUALSENSE_PLAYERLED_3) {
+                Serial.println("led 3 on");
+            } else {
+                Serial.println("led 3 off");
+            }
+            if ((data.player_leds & DUALSENSE_PLAYERLED_4) == DUALSENSE_PLAYERLED_4) {
+                Serial.println("led 4 on");
+            } else {
+                Serial.println("led 4 off");
+            }
+            if ((data.player_leds & DUALSENSE_PLAYERLED_5) == DUALSENSE_PLAYERLED_5) {
+                Serial.println("led 5 on");
+            } else {
+                Serial.println("led 5 off");
+            }
         }
-        if ((data.player_leds & DUALSENSE_PLAYERLED_2) == DUALSENSE_PLAYERLED_2) {
-            Serial.println("led 2 on");
-        } else {
-            Serial.println("led 2 off");
-        }
-        if ((data.player_leds & DUALSENSE_PLAYERLED_3) == DUALSENSE_PLAYERLED_3) {
-            Serial.println("led 3 on");
-        } else {
-            Serial.println("led 3 off");
-        }
-        if ((data.player_leds & DUALSENSE_PLAYERLED_4) == DUALSENSE_PLAYERLED_4) {
-            Serial.println("led 4 on");
-        } else {
-            Serial.println("led 4 off");
-        }
-        if ((data.player_leds & DUALSENSE_PLAYERLED_5) == DUALSENSE_PLAYERLED_5) {
-            Serial.println("led 5 on");
-        } else {
-            Serial.println("led 5 off");
-        }
+    }
+    if (data.lightbar_red!=ledcolor[0] || data.lightbar_green!=ledcolor[1] || data.lightbar_blue!=ledcolor[2]){
+        ledcolor[0]=data.lightbar_red;
+        ledcolor[1]=data.lightbar_green;
+        ledcolor[2]=data.lightbar_blue;
+        Serial.println(String("LED color change, R: ") + ledcolor[0] + " G: " + ledcolor[1] + " B: " + ledcolor[2]);
+    }
+    if (data.mute_button_led!=muteled)
+    {
+        muteled = data.mute_button_led;
+        Serial.println(String("Mute button change: ")+muteled);
     }
 }
 void OnVibrateEvent(DualsenseGamepadOutputReportData data)
@@ -49,13 +65,12 @@ void OnVibrateEvent(DualsenseGamepadOutputReportData data)
     } else {
         digitalWrite(ledPin, HIGH);
     }
-    if (data.player_leds != playerLEDs) {
-        playerLEDs = data.player_leds;
-        OnPlayerLEDChange(data);
+    if (data.lightbar_setup>0 || data.lightbar_red>0 || data.lightbar_green>0 || data.lightbar_red>0 || data.mute_button_led>0 || data.player_leds>0){
+        onLEDsetup(data);
     }
-    Serial.println("Output event. Weak motor: " + String(data.motor_left) + " Strong motor: " + String(data.motor_right) + " Mute LED: " + String(data.mute_button_led) + " LED R: " + String(data.lightbar_red) + " G: " + String(data.lightbar_green) + " B: " + String(data.lightbar_blue));
-}
+    Serial.println("Output event. Weak motor: " + String(data.motor_left) + " Strong motor: " + String(data.motor_right) + "LED change:" + data.lightbar_setup);
 
+}
 void setup()
 {
     Serial.begin(115200);
